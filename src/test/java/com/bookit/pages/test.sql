4e5638becd9b57d3eae0636ae742364e03596aa4
Erkan <ErkanOzulu@users.noreply.github.com>
2023-04-13 22:14:03 +0300

    added new query for Hw

--- src/test/java/com/bookit/pages/test.sql
+++ src/test/java/com/bookit/pages/test.sql
@@ -1,2 +1,4 @@
-select firstname, lastname, role from users
+select firstname, lastname, role, email from users
 where  email = 'blyst6@si.edu';
+
+select firstname||' '|| lastname as name,role,t.batch_number,c.location as campus,t.name as teamname from users u join team t on u.team_id= t.id join campus c on t.campus_id= c.id where u.id=42 ;
